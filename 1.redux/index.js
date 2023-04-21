1898480cfbfe9947278e3dee239136f23444b356
jasmine0714 <woooooooow22@gmail.com>
2023-04-21 22:13:48 +0900

    ✨ reducer로 prevState를 변화시킴. dispatch로 action을 전달함.

--- 1.redux/index.js
+++ 1.redux/index.js
@@ -1,6 +1,26 @@
-const { createStore } = require("redux");
+const { legacy_createStore } = require("redux");
 
-const reducer = () => {};
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case "CHANGE_COMP_A":
+      return {
+        ...prevState,
+        compA: action.data,
+      };
+    case "CHANGE_COMP_B":
+      return {
+        ...prevState,
+        compB: action.data,
+      };
+    case "CHANGE_COMP_C":
+      return {
+        ...prevState,
+        compC: action.data,
+      };
+    default:
+      return prevState;
+  }
+};
 
 const initialState = {
   compA: "a",
@@ -9,5 +29,17 @@ const initialState = {
 };
 
 const store = legacy_createStore(reducer, initialState);
+
+console.log(store.getState());
+
+const changeCompA = (data) => {
+  return {
+    type: "CHANGE_COMP_A",
+    data: data,
+  };
+};
+
+store.dispatch(changeCompA("b"));
+store.dispatch(changeCompA("c"));
 
 console.log(store.getState());
